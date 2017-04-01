f8c1c9db1a41f90ddd7c5ef6ee07fd12e0f8a105
sherlock-y <yb_cswhu@aliyun.com>
2017-04-02 00:28:11 +0800

    reformat code

--- src/main/java/org/sherlocky/leetcode/string/longestSubstringWithoutRepeatingCharacters/SolutionFaster.java
+++ src/main/java/org/sherlocky/leetcode/string/longestSubstringWithoutRepeatingCharacters/SolutionFaster.java
@@ -5,42 +5,42 @@ import java.util.Map;
 
 /**
  * @author yangbing
- * @since 2017-4-1
  * @version 1.0.0
+ * @since 2017-4-1
  */
 public class SolutionFaster {
-	public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() <= 0) {
-	        return 0;
+            return 0;
         }
         if (s.length() == 1) {
-	        return 1;
+            return 1;
         }
         int maxL = -1;
         int start = 0, end = 0, cursor = 1;
         Map<Character, Integer> charToIdx = new HashMap<>();
         charToIdx.put(s.charAt(start), 0);
         while (cursor < s.length()) {
-        	boolean exist = false;
-        	int i = start;
-        	Integer idx = charToIdx.get(s.charAt(cursor));
-        	if (idx != null && idx >= start) {
-	            exist = true;
-	            i = idx;
+            boolean exist = false;
+            int i = start;
+            Integer idx = charToIdx.get(s.charAt(cursor));
+            if (idx != null && idx >= start) {
+                exist = true;
+                i = idx;
             }
-        	charToIdx.put(s.charAt(cursor), cursor);
-        	
-	        if (!exist) {
-	            end++;
+            charToIdx.put(s.charAt(cursor), cursor);
+
+            if (!exist) {
+                end++;
             } else {
-				start = i+1;
-				end = cursor;
-			}
-	        cursor++;
-	        int curL = end - start + 1;
-	        maxL = curL > maxL ? curL: maxL;
+                start = i + 1;
+                end = cursor;
+            }
+            cursor++;
+            int curL = end - start + 1;
+            maxL = curL > maxL ? curL : maxL;
         }
-        
-		return maxL;
+
+        return maxL;
     }
 }
