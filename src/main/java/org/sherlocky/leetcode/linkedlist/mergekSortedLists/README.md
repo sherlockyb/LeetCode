464c60697eef06483525253609b4794d514f1561
sherlock-y <yb_cswhu@aliyun.com>
2017-08-13 17:37:35 +0800

    solution for No.23 problem

---
+++ src/main/java/org/sherlocky/leetcode/linkedlist/mergekSortedLists/README.md
@@ -0,0 +1,7 @@
+### 22. Merge k Sorted Lists
+
+Merge *k* sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+#### 思路（[代码](https://github.com/sherlock-y/LeetCode/blob/master/src/main/java/org/sherlocky/leetcode/linkedlist/mergekSortedLists/Solution.java)）
+
+作为[Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/description/)的扩展，最直接的解法无非就是将2扩展成K即可，但这种解法问题是结果列表每增加一个元素，就需要K次比较，其复杂度为*O(K\*(L1+L2+...+Ln))*，Ln为每个列表的长度。可采用分治法思想，将K的列表拆分成两拨，各自递归拆分合并后，再合并这两个有序列表，时间复杂度为*O(logK\*(L1+L2+...+Ln))*，logK即为K个列表二分拆分的二叉树层级数。
---
+++ src/test/java/org/sherlocky/leetcode/linkedlist/mergeKSortedLists/SolutionTest.java
@@ -0,0 +1,71 @@
+package org.sherlocky.leetcode.linkedlist.mergeKSortedLists;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sherlocky.leetcode.linkedlist.ListNode;
+import org.sherlocky.leetcode.linkedlist.mergekSortedLists.Solution;
+
+import java.util.Arrays;
+
+/**
+ * Created by yangbing on 2017/8/13.
+ */
+public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setup(){
+        solution = new Solution();
+    }
+
+    @Test
+    public void mergeKLists() {
+        ListNode list1 = new ListNode(1);
+        ListNode node12 = new ListNode(3);
+        ListNode node13 = new ListNode(5);
+        list1.next = node12;
+        node12.next = node13;
+
+        ListNode list2 = new ListNode(2);
+        ListNode list22 = new ListNode(6);
+        ListNode list23 = new ListNode(8);
+        list2.next = list22;
+        list22.next = list23;
+
+        // 输出合并结果
+        ListNode p1 = solution.mergeKLists(new ListNode[] {list1, list2});
+        while (p1 != null) {
+            System.out.print(p1.val + " ");
+            p1 = p1.next;
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void mergeKLists2() {
+        ListNode list1 = new ListNode(1);
+        ListNode node12 = new ListNode(3);
+        ListNode node13 = new ListNode(5);
+        list1.next = node12;
+        node12.next = node13;
+
+        ListNode list2 = new ListNode(2);
+        ListNode list22 = new ListNode(6);
+        ListNode list23 = new ListNode(8);
+        list2.next = list22;
+        list22.next = list23;
+
+        ListNode list3 = new ListNode(4);
+        ListNode list32 = new ListNode(7);
+        ListNode list33 = new ListNode(9);
+        list3.next = list32;
+        list32.next = list33;
+        ListNode p1 = solution.mergeKLists(new ListNode[] {list1, list2, list3});
+        // 输出合并结果
+        while (p1 != null) {
+            System.out.print(p1.val + " ");
+            p1 = p1.next;
+        }
+        System.out.println();
+    }
+}
