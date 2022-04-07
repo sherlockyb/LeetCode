0d3133223082229d95ca76ef6d7c628da9221ce9
sherlockyb <yb_cswhu@aliyun.com>
2022-04-07 23:00:57 +0800

    ADD: README for problem 51

---
+++ src/main/java/org/sherlockyb/leetcode/backtracking/nQueens/Solution.java
@@ -0,0 +1,14 @@
+package org.sherlockyb.leetcode.backtracking.nQueens;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author biyang
+ */
+public class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        // TODO
+        return Collections.emptyList();
+    }
+}
